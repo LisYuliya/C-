24687bbb65a0114e8b8c30fbc19e8a0852e348cc
Yuliya <8avokado8@gmail.com>
2022-10-16 21:40:38 +0600

    Еxample003 Sum

--- Еxample003_Sum/Program.cs
+++ Еxample003_Sum/Program.cs
@@ -1,4 +1,14 @@
-﻿int numberA = 31;
-int numberВ = 5;
-int result = numberA + numberВ;
+﻿//int numberA = 31;
+//int numberВ = 5;
+//int result = numberA + numberВ;
+//Console.WriteLine(result);
+
+double numberA = new Random().Next(1,10);
+Console.Write("numberA =");
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.Write("numberB =");
+Console.WriteLine(numberB);
+double result = numberA + numberB;
+Console.Write("numberA+numberB =");
 Console.WriteLine(result);
